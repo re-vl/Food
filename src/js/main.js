960d1bda3c6d6cf112ae94691a982c6d7ebf1fb8
Vladimir <rva7171@gmail.com>
2021-04-11 21:21:24 +0300

    запрос на сервер через axios

--- src/js/main.js
+++ src/js/main.js
@@ -202,7 +202,7 @@ window.addEventListener("DOMContentLoaded", () => {
       return await res.json(); // ждем ответ await
    };
 
-   //Создаем карточки
+   /*  //Создаем карточки
    //получаем массив объекта
    getResource("http://localhost:3000/menu").then((data) => {
       //перебор массива с деструктурезацией по частям
@@ -217,31 +217,46 @@ window.addEventListener("DOMContentLoaded", () => {
             ".menu .container"
          ).render();
       });
+   }); */
+
+   /*  //Тоже но без шаблонизатора по классам
+   getResource('http://localhost:3000/menu')
+       .then(data => createCard(data));
+
+   function createCard(data) {
+       data.forEach(({img, altimg, title, descr, price}) => {
+           const element = document.createElement('div');
+
+           element.classList.add("menu__item");
+
+           element.innerHTML = `
+               <img src=${img} alt=${altimg}>
+               <h3 class="menu__item-subtitle">${title}</h3>
+               <div class="menu__item-descr">${descr}</div>
+               <div class="menu__item-divider"></div>
+               <div class="menu__item-price">
+                   <div class="menu__item-cost">Цена:</div>
+                   <div class="menu__item-total"><span>${price}</span> грн/день</div>
+               </div>
+           `;
+           document.querySelector(".menu .container").append(element);
+       });
+   } */
+
+   // создание карточек с запросом на сервер через
+   axios.get("http://localhost:3000/menu").then((data) => {
+      data.data.forEach(({ img, altimg, title, descr, price }) => {
+         //передаем в конструктор для создания карточки
+         new MenuCard(
+            img,
+            altimg,
+            title,
+            descr,
+            price,
+            ".menu .container"
+         ).render();
+      });
    });
-
-   // Тоже но без шаблонизатора по классам
-   // getResource('http://localhost:3000/menu')
-   //     .then(data => createCard(data));
-
-   // function createCard(data) {
-   //     data.forEach(({img, altimg, title, descr, price}) => {
-   //         const element = document.createElement('div');
-
-   //         element.classList.add("menu__item");
-
-   //         element.innerHTML = `
-   //             <img src=${img} alt=${altimg}>
-   //             <h3 class="menu__item-subtitle">${title}</h3>
-   //             <div class="menu__item-descr">${descr}</div>
-   //             <div class="menu__item-divider"></div>
-   //             <div class="menu__item-price">
-   //                 <div class="menu__item-cost">Цена:</div>
-   //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
-   //             </div>
-   //         `;
-   //         document.querySelector(".menu .container").append(element);
-   //     });
-   // }
 
    //Forms
 
